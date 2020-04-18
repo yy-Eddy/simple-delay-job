0e4c98873cddf28157b7ef26eb5353b234c75492
yang yu <1021015757@qq.com>
2020-04-18 11:42:13 +0800

    删除目前未引入的功能

--- simple-delay-job-service/src/main/java/com/github/yyeddy/sdj/configuration/BroadcastConfig.java
+++ simple-delay-job-service/src/main/java/com/github/yyeddy/sdj/configuration/BroadcastConfig.java
@@ -1,20 +1,16 @@
 package com.github.yyeddy.sdj.configuration;
 
-import com.github.yyeddy.scb.ClusterBroadcast;
-import com.github.yyeddy.scb.redis.JedisBroadcast;
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import redis.clients.jedis.JedisPool;
 
 //@Configuration
 public class BroadcastConfig {
 
-    @Bean(initMethod = "start",destroyMethod = "stop")
+    /*@Bean(initMethod = "start",destroyMethod = "stop")
     public ClusterBroadcast jedisBroadcast(){
         GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
         JedisPool pool = new JedisPool(poolConfig, "127.0.0.1", 6379, 1000, "");
         ClusterBroadcast broadcast = new JedisBroadcast(pool);
         return broadcast;
-    }
+    }*/
 }
